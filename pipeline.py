a63cae4e7e6a1506fa829a9aa76dc53fa792f1ed
theminer3746 <me@theminerdev.com>
2021-05-19 07:56:40 +0000

    Init project

---
+++ pipeline.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import time
+from subprocess import Popen, PIPE 
+
+apk_path = "../vulpix-runner/automated-gui-tester/apk"
+log_path = "new_log"
+
+def flowDroid(apk):
+    start_time = time.time()
+    p = Popen(["java", "-jar", "soot-infoflow-cmd-jar-with-dependencies.jar", "-a", apk_path+"\\"+apk,"-p","home/theminer3746/Android/Sdk/platforms","-s","mergeSuSi.txt","-ct","120"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate() 
+    rc = p.returncode
+    finish_time = time.time()
+    time_lapsed = finish_time - start_time
+    fp = open(log_path + "\\" + apk + ".log","w")
+    fp.write("Time lapsed: " + str(time_lapsed) + (" s\n\n"))
+    fp.write(str(err,"utf-8"))
+    fp.close()
+
+
+flowDroid("com.ookbee.ookbeecomics.android.apk")
